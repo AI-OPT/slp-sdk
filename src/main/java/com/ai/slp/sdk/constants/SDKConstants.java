91b8b0ab90d165b785d4d393e77c59180687a525
zhangchao2 <zhangchao2@asiainfo.com>
2016-04-21 17:15:50 +0800

    新增，数据源从配置中心获取

--- src/main/java/com/ai/slp/sdk/constants/SDKConstants.java
+++ src/main/java/com/ai/slp/sdk/constants/SDKConstants.java
@@ -12,4 +12,6 @@ public final class SDKConstants {
     // 技术服务与密码的映射关系 {"TXS001":"password"}
     public static final String SERVICE_PWD_MAPPED_PATH = "/com/ai/slp/service-pwd-mapped";
 
+    // 数据源的配置 {"db1":{},"db2":{}}
+    public static final String DATASOURCES_PATH = "/com/ai/slp/datasources";
 }
--- src/main/java/com/ai/slp/sdk/datasource/OptHikariDataSource.java
+++ src/main/java/com/ai/slp/sdk/datasource/OptHikariDataSource.java
@@ -1,12 +1,41 @@
 package com.ai.slp.sdk.datasource;
 
+import com.ai.paas.ipaas.ccs.constants.ConfigException;
+import com.ai.slp.sdk.components.ccs.CCSFactory;
+import com.ai.slp.sdk.constants.SDKConstants;
+import com.ai.slp.sdk.exception.SDKException;
+import com.ai.slp.sdk.util.StringUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 public class OptHikariDataSource extends HikariDataSource {
 
     public OptHikariDataSource(String dataSourceName) {
-        // 从paas中获取数据库配置信息，并初始化数据源
-        // super(OptConfHelper.getInstance().getDBConf(dataSourceName));
+        super(getDBConf(dataSourceName));
+    }
+
+    private static HikariConfig getDBConf(String dataSourceName) {
+        String data;
+        try {
+            data = CCSFactory.getDefaultConfigClient().get(SDKConstants.DATASOURCES_PATH);
+        } catch (ConfigException e) {
+            throw new SDKException("get database conf error from path["
+                    + SDKConstants.DATASOURCES_PATH + "]", e);
+        }
+        if (StringUtil.isBlank(data)) {
+            throw new SDKException("cann't get database conf from path["
+                    + SDKConstants.DATASOURCES_PATH + "]");
+        }
+        JSONObject dbConfJson = JSONObject.parseObject(data);
+        JSONObject confObject = (JSONObject) dbConfJson.get(dataSourceName);
+        if (confObject == null) {
+            throw new SDKException("cann't get database config info of dataSourceName["
+                    + dataSourceName + "]");
+        }
+        HikariConfig dbconf = JSONObject.toJavaObject(confObject, HikariConfig.class);
+
+        return dbconf;
     }
 
 }
